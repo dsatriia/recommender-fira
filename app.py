a4c11e292b7c451343b27e1a1a8974e8f61b59d7
firaapriliana <42562317+firaapriliana@users.noreply.github.com>
2022-02-01 08:09:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -682,13 +682,13 @@ def main():
 							#short paling besar input ke num_topics=bigest
 
 							#mengambil nilai dalam array
-							max_Coherence = np.argmax(coherence_scores)
+# 							max_Coherence = np.argmax(coherence_scores)
 							
 # 							st.write("numb of topic:",max_Coherence)
 # 							st.write("best coherence score:",coherence_scores[max_Coherence])
 
 							lsi_model = LsiModel(
-								corpus=corpus_tfidf, id2word=dictionary, num_topics=max_Coherence
+								corpus=corpus_tfidf, id2word=dictionary, num_topics=6
 							)
 							print(
 								"Derivation of Term Matrix T of Training Document Word Stems: ",
@@ -903,13 +903,13 @@ def main():
 # 						short paling besar input ke num_topics=bigest
 
 						#mengambil nilai dalam array
-							max_Coherence = np.argmax(coherence_scores)
+# 							max_Coherence = np.argmax(coherence_scores)
 
-							st.write("numb of topic:",max_Coherence)
-							st.write("best coherence score:",coherence_scores[max_Coherence])
+# 							st.write("numb of topic:",max_Coherence)
+# 							st.write("best coherence score:",coherence_scores[max_Coherence])
 
 						lsi_model = LsiModel(
-							corpus=corpus_tfidf, id2word=dictionary, num_topics=max_Coherence
+							corpus=corpus_tfidf, id2word=dictionary, num_topics=6
 						)
 						print(
 							"Derivation of Term Matrix T of Training Document Word Stems: ",
