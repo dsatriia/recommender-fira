f97ccabc7dc364d1db63a44716be6b6271168720
firaapriliana <42562317+firaapriliana@users.noreply.github.com>
2022-02-28 01:47:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -668,7 +668,7 @@ def main():
 
 							#bmemilih num_topics optimal sesuai topic coherence
 						
-							k_topics = [1,2,3,4,5,6,7,8,9,10,15,20,25,30]
+							k_topics = [5,10,15,20,25,30,35,40,45,50]
 							coherence_scores = []
 
 							for i in k_topics:
@@ -889,7 +889,7 @@ def main():
 						corpus_tfidf = tfidf[corpus]
 
 						#bmemilih num_topics optimal sesuai topic coherence
-						k_topics = [1,2,3,4,5,6,7,8,9,10,15,20,25,30]
+						k_topics = [5,10,15,20,25,30,35,40,45,50]
 						coherence_scores = []
 
 						for i in k_topics:
